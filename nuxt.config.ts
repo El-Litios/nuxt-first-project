acb55aa6cc1bedcf0bb7bd4ba8d9bdcf050ed176
Carlos Guzmán Rojas <63490179+El-Litios@users.noreply.github.com>
2023-12-05 09:46:46 -0300

    content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { resolve } from 'path'
 
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   alias: {
     /* '@': resolve(__dirname, "/") */
     assets: '/<rootDir>/assets'
   },
-  css: ["~/assets/main.scss"]
+  css: ["~/assets/main.scss"],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/content',]
 })
