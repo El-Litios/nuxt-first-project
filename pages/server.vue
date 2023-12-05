4566e671ca6641c136f7e22f339f0c2408a08f51
Carlos Guzmán Rojas <63490179+El-Litios@users.noreply.github.com>
2023-12-05 12:39:51 -0300

    server

---
+++ pages/server.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+const response = await $fetch('/api/hello');
+
+console.log(response);
+
+</script>
+
+<style>
+
+</style>
